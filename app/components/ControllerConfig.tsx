e30cd95c43ea94e405c8cb28c6ef2dbc8bbdf8a6
iain maitland <github@iainmaitland.com>
2025-05-02 15:28:43 -0400

    working experiment controller form

--- app/components/ControllerConfig.tsx
+++ app/components/ControllerConfig.tsx
@@ -3,7 +3,9 @@ import {
   AdjustmentsVerticalIcon,
 } from "@heroicons/react/24/outline";
 
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useParams, useSubmit } from "@remix-run/react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 import SchemaForm from "./SchemaForm";
 import { loader } from "~/routes/devices.$id.$name.experiments.$experiment_id.$controller_id.config";
 
@@ -12,20 +14,56 @@ type ControllerConfigProps = {
     classinfo: string;
     config: Record<string, unknown>;
   };
+  actionData?: any;
 };
 
-export function ControllerConfig({ controller }: ControllerConfigProps) {
-  const controllerName = controller.config.name;
+export function ControllerConfig({ controller, actionData }: ControllerConfigProps) {
+  const controllerName = controller.config.name as string;
   const loaderData = useLoaderData<typeof loader>();
+  const { id, experiment_id, controller_id } = useParams();
+  const submit = useSubmit();
+
+  // Display any error messages from the action
+  useEffect(() => {
+    if (actionData?.error) {
+      if (typeof actionData.error === "string") {
+        toast.error(actionData.error);
+      }
+      if (typeof actionData.error === "object") {
+        const errorMessages: string[] = [];
+        Object.entries(actionData.error).forEach(([key, value]) => {
+          errorMessages.push(`${key}: ${value}`);
+        });
+        errorMessages.forEach((message) => {
+          toast.error(message);
+        });
+      }
+    }
+  }, [actionData]);
+
+  const handleSubmit = (data: any) => {
+    toast.dismiss();
+    
+    const formData = new FormData();
+    formData.append("intent", "update_controller");
+    formData.append("id", id || "");
+    formData.append("experiment_id", experiment_id || "");
+    formData.append("controller_id", controller_id || "");
+    formData.append("controller_config", JSON.stringify(data));
+    
+    submit(formData, {
+      method: "POST",
+    });
+  };
 
   return (
     <div id={controllerName + "config"}>
       <SchemaForm
         schema={loaderData.classinfoSchema.config}
         formData={controller.config}
-        onSubmit={(data) => console.log("Submitted data", data)}
-        title="Configuration Form"
-        submitText="Save"
+        onSubmit={handleSubmit}
+        title="Controller Configuration"
+        submitText="Save Changes"
       />
     </div>
   );
