b5275353a5b8e5b277192e702d30239e2184d940
iain maitland <github@iainmaitland.com>
2025-05-07 15:13:23 -0400

    local ObjectFieldTemplate.tsx

--- app/components/SchemaForm.tsx
+++ app/components/SchemaForm.tsx
@@ -1,14 +1,124 @@
 import Form from "@rjsf/core";
 import pkg from "@rjsf/utils";
-const { getInputProps, getSubmitButtonOptions } = pkg;
+const {
+  getInputProps,
+  getSubmitButtonOptions,
+  canExpand,
+  descriptionId,
+  getTemplate,
+  getUiOptions,
+  titleId,
+  buttonId,
+} = pkg;
 
 type BaseInputTemplateProps = pkg.BaseInputTemplateProps;
 type FieldTemplateProps = pkg.FieldTemplateProps;
 type RJSFSchema = pkg.RJSFSchema;
 type SubmitButtonProps = pkg.SubmitButtonProps;
-import ObjectFieldTemplate from "./daisyui/ObjectFieldTemplate";
+type ObjectFieldTemplateProps = pkg.ObjectFieldTemplateProps;
+type FormContextType = pkg.FormContextType;
+type StrictRJSFSchema = pkg.StrictRJSFSchema;
+
 import validator from "@rjsf/validator-ajv8";
 import { ChangeEvent, FocusEvent } from "react";
+
+function ObjectFieldTemplate<
+  T = unknown,
+  S extends StrictRJSFSchema = RJSFSchema,
+  F extends FormContextType = Record<string, unknown>,
+>(props: ObjectFieldTemplateProps<T, S, F>) {
+  const {
+    description,
+    title,
+    properties,
+    required,
+    disabled,
+    readonly,
+    uiSchema,
+    idSchema,
+    schema,
+    formData,
+    onAddClick,
+    registry,
+  } = props;
+  const uiOptions = getUiOptions<T, S, F>(uiSchema);
+  const TitleFieldTemplate = getTemplate<"TitleFieldTemplate", T, S, F>(
+    "TitleFieldTemplate",
+    registry,
+    uiOptions,
+  );
+  const DescriptionFieldTemplate = getTemplate<
+    "DescriptionFieldTemplate",
+    T,
+    S,
+    F
+  >("DescriptionFieldTemplate", registry, uiOptions);
+  // Button templates are not overridden in the uiSchema
+  const {
+    ButtonTemplates: { AddButton },
+  } = registry.templates;
+
+  // Check if this is the root object
+  const isRoot = idSchema.$id === "root";
+
+  return (
+    <div className={`form-control ${isRoot ? "" : ""}`}>
+      {title && (
+        <div className="card-title font-mono">
+          <TitleFieldTemplate
+            id={titleId<T>(idSchema)}
+            title={title}
+            required={required}
+            schema={schema}
+            uiSchema={uiSchema}
+            registry={registry}
+          />
+        </div>
+      )}
+      {description && (
+        <DescriptionFieldTemplate
+          id={descriptionId<T>(idSchema)}
+          description={description}
+          schema={schema}
+          uiSchema={uiSchema}
+          registry={registry}
+        />
+      )}
+      <div
+        className={`grid grid-cols-3 gap-${description ? 3 : 4} ${isRoot ? "" : "mb-4"}`}
+      >
+        {properties.map((element, index) =>
+          element.hidden ? (
+            element.content
+          ) : (
+            <div
+              key={`${idSchema.$id}-${element.name}-${index}`}
+              className={
+                idSchema.$id === "root" && element.name === "tasks"
+                  ? "mt-2"
+                  : ""
+              }
+            >
+              {element.content}
+            </div>
+          ),
+        )}
+        {canExpand<T, S, F>(schema, uiSchema, formData) && (
+          <div className="flex justify-end">
+            <AddButton
+              id={buttonId<T>(idSchema, "add")}
+              className="rjsf-object-property-expand btn btn-primary btn-sm"
+              onClick={onAddClick(schema)}
+              disabled={disabled || readonly}
+              uiSchema={uiSchema}
+              registry={registry}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
 function CustomFieldTemplate(props: FieldTemplateProps) {
   const { classNames, style, help, errors, children } = props;
